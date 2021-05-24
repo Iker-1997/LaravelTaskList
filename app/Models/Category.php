6028badc24946252943b03399a424dfe85ec2054
Iker Cayero <iker.1997.19@gmail.com>
2021-05-24 18:16:27 +0200

    Models modifies

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Task extends Model
+class Category extends Model
 {
     use HasFactory;
 
-    public function category(){
-        return $this->belongsTo(Category::class, 'cat_id');
+    public function tasks(){
+        return $this->hasMany(Task::class, 'cat_id');
     }
 }
